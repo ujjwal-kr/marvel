d6e3feb4d505e1a2e26562dd959767e62f29a499
Ujjwal <katanadeveloper01@gmail.com>
2019-06-04 14:52:09 +0530

    added bulma

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyBkGthA4O7YnE_idTKEkdXG0-NNSyefcYY',
+    authDomain: 'marvel-init.firebaseapp.com',
+    databaseURL: 'https://marvel-init.firebaseio.com',
+    projectId: 'marvel-init',
+    storageBucket: 'marvel-init.appspot.com',
+    messagingSenderId: '963672014094',
+    appId: '1:963672014094:web:0fc6b8064e1ef170'
+  }
 };
