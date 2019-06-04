e0608fa8b68c8b2cb2ca1351bd2ceccfef017825
Ujjwal <katanadeveloper01@gmail.com>
2019-06-05 00:41:16 +0530

    1

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [MatToolbarModule],
+  exports: [MatToolbarModule]
 })
 
 export class MaterialModule {}
