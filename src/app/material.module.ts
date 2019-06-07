8a85b798c89b4cb3983dd0b84019edd737a70551
Ujjwal <katanadeveloper01@gmail.com>
2019-06-07 20:24:34 +0530

    finished the showcase section

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   imports: [
     MatToolbarModule,
+    MatButtonModule
   ],
   exports: [
     MatToolbarModule,
+    MatButtonModule
   ]
 })
 
